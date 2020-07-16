129c4f28814134615b25f94e543adf972cae4ad4
Lily <lilyyanglt@gmail.com>
2020-07-16 11:44:13 -0700

    Added sorting by price for ResultList

--- client/src/components/ResultList.js
+++ client/src/components/ResultList.js
@@ -4,6 +4,13 @@ const ResultList = ({items}) => {
 
   console.log("results list being called");
   console.log("------------------------");
+
+  // sort by price when displaying the items
+
+  items.sort((item1, item2) => {
+    return Number(item1.itemPrice) > Number(item2.itemPrice) ? 1 : -1;
+  })
+
   return items.map(item => <Card key={item.id} content={item}/>)
 }
 
@@ -11,10 +18,9 @@ const Card = React.memo(({content}) => {
   console.log("calling Card");
   console.log("---------------");
 
-  // sort by price before displaying
-
-  return <div>{content.itemName}
+  return <div className="item-card">
     <span>{content.storeName}</span>
+    <span>{content.itemName}</span>
     <span>{content.itemPrice}</span>
     <span>{content.itemDesc}</span>
   </div>
