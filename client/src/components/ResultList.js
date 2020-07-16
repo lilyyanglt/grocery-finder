1e40c6705218cdf1923edce028cd4338647292e0
Lily <lilyyanglt@gmail.com>
2020-07-16 11:35:58 -0700

    Created memo version of Card

--- client/src/components/ResultList.js
+++ client/src/components/ResultList.js
@@ -7,14 +7,17 @@ const ResultList = ({items}) => {
   return items.map(item => <Card key={item.id} content={item}/>)
 }
 
-const Card = ({content}) => {
+const Card = React.memo(({content}) => {
   console.log("calling Card");
   console.log("---------------");
+
+  // sort by price before displaying
+
   return <div>{content.itemName}
     <span>{content.storeName}</span>
     <span>{content.itemPrice}</span>
     <span>{content.itemDesc}</span>
   </div>
-}
+})
 
 export default ResultList;
