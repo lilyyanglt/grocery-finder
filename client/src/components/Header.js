26633283801b9274d0db515527ec2b5b81bc5ea7
Lily <lilyyanglt@gmail.com>
2020-07-29 22:26:08 -0700

    Added style for header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import style from '../style/header.module.css';
+import githubIcon from '../asset/github.svg';
 
 function Header() {
-  return <header className="header"><h1 className="header_title">Grocery App</h1></header>
+  return (
+    <header className={style.header}>
+      <h1 className={style.appName}>Grocery Finder</h1>
+      <div>
+        <p className={style.note}>Made with ❤️ by Lily Yang</p>
+        <a href="https://github.com/lilyyanglt/grocery-search-engine/"><img src={githubIcon} className={style.icon}/></a>
+      </div>
+    </header>
+  )
 }
 
 export default Header;
