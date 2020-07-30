d9a71b38f9f8475851d84c73c168657c285e1018
Lily <lilyyanglt@gmail.com>
2020-07-30 09:24:35 -0700

    Added footer and style

---
+++ client/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+        <p className="footer">Made with ❤️ by Lily Yang &copy; 2020</p>
+    </footer>
+  )
+}
+
+export default Footer;
