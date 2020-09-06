84a9491017e68b6d29d8b2d44776b1a65fa9dcbd
Lily <lilyyanglt@gmail.com>
2020-09-06 15:47:36 -0700

    Added route to user to adding to shopping list

--- server/src/routes/user/index.js
+++ server/src/routes/user/index.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const passport = require('passport');
+const User = require('../../model/userSchema');
 
 router.get('/', (req, res) => {
   res.json({message: `${req.url} is valid`});
@@ -56,6 +57,44 @@ router.get('/loginStat', (req, res) => {
       userLoggedOutSuccess: true
     })
   }
+})
+
+// handle for updating user shopping list with items
+
+router.put('/addItem/:id', (req, res, next) => {
+  const newItem = req.body
+  const userId = req.params.id
+  console.log(newItem);
+  User.findOne({_id: userId}, (err, user) => {
+    if (user) {
+      const currentList = user.shoppingList
+      currentList.push(newItem._id);
+      User.findOneAndUpdate({_id: userId}, {shoppingList: currentList})
+      .then(user => {
+        if (user) {
+          res.json({
+            message: "Successfully added",
+            listUpdated: true
+          })
+        } else {
+          res.json({
+            message: "Added failure",
+            listUpdated: false
+          })
+        }
+      })
+      .catch(err => next(err))
+     
+    } else {
+      next(err)
+    }
+  })
+
+  // find user in db based on id
+  // get the new item from the req.body
+  // once found, take current shopping list
+  // append new item to current shopping list
+  // update user's shopping list with the new list
 })
 
 module.exports = router;
